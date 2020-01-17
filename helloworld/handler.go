da12c852ce68084b2ba48be34aa545dc260ab33f
marami52 <marami@ironix.com>
2020-01-17 18:33:28 +0000

    init

---
+++ helloworld/handler.go
@@ -0,0 +1,10 @@
+package function
+
+import (
+	"fmt"
+)
+
+// Handle a serverless request
+func Handle(req []byte) string {
+	return fmt.Sprintf("ashkan52 - ba ba blackship ....12 You said: %s", string(req))
+}
